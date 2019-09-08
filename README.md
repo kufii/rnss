b6cf7995f2f9568746677e012cbbee9df75918f2
Adrien Pyke <adpyke@gmail.com>
2019-09-08 00:50:16 -0400

    prettier, fix vulnerability

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # rnss
+
 Better style sheets for React Native
 
 ## Installation
+
 `npm install --save rnss`
 
 ## Usage
+
 rnss is a wrapper for `StyleSheet.create`. Styles are created in template strings, and rules are separated by new lines or semi colons.
+
 ```javascript
 import r from 'rnss';
 
@@ -20,72 +24,78 @@ const containerStyle = r`
 ```
 
 ## Property Names
+
 style property names can be written in the following ways, camelCase, dash-case, or shorthands. So the following are all equivalent.
+
 ```javascript
-r`flex 1; flex-direction row; background-color #fff`
+r`flex 1; flex-direction row; background-color #fff`;
 
-r`flex 1; flexDirection row; backgroundColor #fff`
+r`flex 1; flexDirection row; backgroundColor #fff`;
 
-r`f 1; fd row; bc #fff`
+r`f 1; fd row; bc #fff`;
 ```
 
 ## Create a StyleSheet
+
 If you want to create a full stylesheet you can use the function version of `r` and pass in an object.
 
 ```javascript
 const styles = r({
-    container: `flex 1; flex-direction row`,
-    text: `margin 5; font-weight bold`
+	container: `flex 1; flex-direction row`,
+	text: `margin 5; font-weight bold`
 });
 ```
 
 ## Helpers
+
 rnss ships with the following css style headers:
 
 **margin/m, padding/p, border-width/bw**  
 You can use these three helpers to set the corresponding property on all four sides.
 
 ```javascript
-r`m 1 2 3 4` // === { marginTop: 1, marginRight: 2, marginBottom: 3, marginLeft: 4 }
-r`m 1 2 3` // === { marginTop: 1, marginRight: 2, marginBottom: 2, marginLeft: 2 }
-r`m 1 2` // === { marginTop: 1, marginRight: 2, marginBottom: 1, marginLeft: 2 }
-r`m 1` // === { marginTop: 1, marginRight: 1, marginBottom: 1, marginLeft: 1 }
+r`m 1 2 3 4`; // === { marginTop: 1, marginRight: 2, marginBottom: 3, marginLeft: 4 }
+r`m 1 2 3`; // === { marginTop: 1, marginRight: 2, marginBottom: 2, marginLeft: 2 }
+r`m 1 2`; // === { marginTop: 1, marginRight: 2, marginBottom: 1, marginLeft: 2 }
+r`m 1`; // === { marginTop: 1, marginRight: 1, marginBottom: 1, marginLeft: 1 }
 ```
 
 **border-radius/br**
 This works similarly to the previous helpers but sets the `borderRadius` property on all 4 corners.
 
 ```javascript
-r`br 1 2 3 4` // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 3, borderBottomLeftRadius: 4 }
-r`br 1 2 3` // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 3, borderBottomLeftRadius: 2 }
-r`br 1 2` // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 1, borderBottomLeftRadius: 2 }
-r`br 1` // === { borderTopLeftRadius: 1, borderTopRightRadius: 1, borderBottomRightRadius: 1, borderBottomLeftRadius: 1 }
+r`br 1 2 3 4`; // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 3, borderBottomLeftRadius: 4 }
+r`br 1 2 3`; // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 3, borderBottomLeftRadius: 2 }
+r`br 1 2`; // === { borderTopLeftRadius: 1, borderTopRightRadius: 2, borderBottomRightRadius: 1, borderBottomLeftRadius: 2 }
+r`br 1`; // === { borderTopLeftRadius: 1, borderTopRightRadius: 1, borderBottomRightRadius: 1, borderBottomLeftRadius: 1 }
 ```
 
 **border/b**
 This helper is used to set the `borderWidth`, `borderStyle`, and `borderColor` properties simultaneously.
 
 ```javascript
-r`b 1 solid black` // === { borderWidth: 1, borderStyle: 'solid', borderColor: 'black' }
+r`b 1 solid black`; // === { borderWidth: 1, borderStyle: 'solid', borderColor: 'black' }
 ```
 
 ## Hairline Width
+
 `hairlineWidth`, `hairline-width`, or `hw` will be replaced by `StyleSheet.hairlineWidth`. So the following are equivalent:
 
 ```javascript
-r`border-width ${StyleSheet.hairlineWidth}`
-r`border-width hw`
+r`border-width ${StyleSheet.hairlineWidth}`;
+r`border-width hw`;
 ```
 
 ## Custom Helpers
+
 Along side the built in helpers, you can also create your own custom helpers using the `r.helper` function.
 
 ```javascript
 r.helper({
-    flexRow: `f 1; fd row`,
-    bigMargin: num => r`m ${num * 10}`
+	flexRow: `f 1; fd row`,
+	bigMargin: num => r`m ${num * 10}`
 });
 
-r`flex-row` // === { flex: 1, flexDirection: 'row' }
-r`big-margin 5` // === { margin: 50 }
+r`flex-row`; // === { flex: 1, flexDirection: 'row' }
+r`big-margin 5`; // === { margin: 50 }
 ```
--- index.js
+++ index.js
@@ -1,10 +1,118 @@
 import { StyleSheet } from 'react-native';
 
-const props = ['alignContent', 'alignItems', 'alignSelf', 'aspectRatio', 'borderBottomWidth', 'borderLeftWidth', 'borderRightWidth', 'borderTopWidth', 'borderEndWidth', 'borderStartWidth', 'borderWidth', 'bottom', 'direction', 'display', 'end', 'start', 'flex', 'flexDirection', 'flexBasis', 'flexGrow', 'flexShrink', 'flexWrap', 'height', 'justifyContent', 'left', 'margin', 'marginBottom', 'marginHorizontal', 'marginLeft', 'marginRight', 'marginTop', 'marginVertical', 'marginEnd', 'marginStart', 'maxHeight', 'maxWidth', 'minHeight', 'minWidth', 'padding', 'paddingBottom', 'paddingHorizontal', 'paddingLeft', 'paddingRight', 'paddingTop', 'paddingVertical', 'paddingEnd', 'paddingStart', 'position', 'right', 'top', 'width', 'zIndex', 'shadowColor', 'shadowOffset', 'shadowOpacity', 'shadowRadius', 'decomposedMatrix', 'transform', 'transformMatrix', 'backfaceVisibility', 'backgroundColor', 'borderBottomLeftRadius', 'borderBottomRightRadius', 'borderColor', 'borderRadius', 'borderTopLeftRadius', 'borderTopRightRadius', 'opacity', 'overflow', 'resizeMode', 'tintColor', 'overlayColor', 'borderBottomColor', 'borderLeftColor', 'borderRightColor', 'borderStyle', 'borderTopColor', 'elevation', 'color', 'fontFamily', 'fontSize', 'fontStyle', 'fontVariant', 'textTransform', 'fontWeight', 'includeFontPadding', 'lineHeight', 'textAlign', 'textDecorationLine', 'textShadowColor', 'textShadowOffset', 'textShadowRadius', 'textAlignVertical', 'letterSpacing', 'textDecorationColor', 'textDecorationStyle', 'writingDirection', 'borderBottomEndRadius', 'borderBottomStartRadius', 'borderEndColor', 'borderStartColor', 'borderTopEndRadius', 'borderTopStartRadius'];
+const props = [
+	'alignContent',
+	'alignItems',
+	'alignSelf',
+	'aspectRatio',
+	'borderBottomWidth',
+	'borderLeftWidth',
+	'borderRightWidth',
+	'borderTopWidth',
+	'borderEndWidth',
+	'borderStartWidth',
+	'borderWidth',
+	'bottom',
+	'direction',
+	'display',
+	'end',
+	'start',
+	'flex',
+	'flexDirection',
+	'flexBasis',
+	'flexGrow',
+	'flexShrink',
+	'flexWrap',
+	'height',
+	'justifyContent',
+	'left',
+	'margin',
+	'marginBottom',
+	'marginHorizontal',
+	'marginLeft',
+	'marginRight',
+	'marginTop',
+	'marginVertical',
+	'marginEnd',
+	'marginStart',
+	'maxHeight',
+	'maxWidth',
+	'minHeight',
+	'minWidth',
+	'padding',
+	'paddingBottom',
+	'paddingHorizontal',
+	'paddingLeft',
+	'paddingRight',
+	'paddingTop',
+	'paddingVertical',
+	'paddingEnd',
+	'paddingStart',
+	'position',
+	'right',
+	'top',
+	'width',
+	'zIndex',
+	'shadowColor',
+	'shadowOffset',
+	'shadowOpacity',
+	'shadowRadius',
+	'decomposedMatrix',
+	'transform',
+	'transformMatrix',
+	'backfaceVisibility',
+	'backgroundColor',
+	'borderBottomLeftRadius',
+	'borderBottomRightRadius',
+	'borderColor',
+	'borderRadius',
+	'borderTopLeftRadius',
+	'borderTopRightRadius',
+	'opacity',
+	'overflow',
+	'resizeMode',
+	'tintColor',
+	'overlayColor',
+	'borderBottomColor',
+	'borderLeftColor',
+	'borderRightColor',
+	'borderStyle',
+	'borderTopColor',
+	'elevation',
+	'color',
+	'fontFamily',
+	'fontSize',
+	'fontStyle',
+	'fontVariant',
+	'textTransform',
+	'fontWeight',
+	'includeFontPadding',
+	'lineHeight',
+	'textAlign',
+	'textDecorationLine',
+	'textShadowColor',
+	'textShadowOffset',
+	'textShadowRadius',
+	'textAlignVertical',
+	'letterSpacing',
+	'textDecorationColor',
+	'textDecorationStyle',
+	'writingDirection',
+	'borderBottomEndRadius',
+	'borderBottomStartRadius',
+	'borderEndColor',
+	'borderStartColor',
+	'borderTopEndRadius',
+	'borderTopStartRadius'
+];
 
-const initials = str => str.match(/^[a-z]|[A-Z]/g).join('').toLowerCase();
+const initials = str =>
+	str
+		.match(/^[a-z]|[A-Z]/g)
+		.join('')
+		.toLowerCase();
 
-const shorts = props.reduce((obj, prop) => (obj[initials(prop)] = prop, obj), {});
+const shorts = props.reduce((obj, prop) => ((obj[initials(prop)] = prop), obj), {});
 
 Object.assign(shorts, {
 	bc: 'backgroundColor',
@@ -20,15 +128,17 @@ Object.assign(shorts, {
 	t: 'top'
 });
 
-const zip = (parts, args) => parts.reduce((acc, c, i) => acc + c + (args[i] == null ? '' : args[i]), '');
+const zip = (parts, args) =>
+	parts.reduce((acc, c, i) => acc + c + (args[i] == null ? '' : args[i]), '');
 
 const memo = (fn, cache = {}) => x => cache[x] || (cache[x] = fn(x));
 
 const dedash = str => str.replace(/-([a-z])/, (_, value) => value.toUpperCase());
 
-const numOrStr = str => isNaN(str) ? str : parseFloat(str);
+const numOrStr = str => (isNaN(str) ? str : parseFloat(str));
 
-const assignProps = (value, ...props) => props.reduce((obj, prop) => (obj[prop] = value, obj), {});
+const assignProps = (value, ...props) =>
+	props.reduce((obj, prop) => ((obj[prop] = value), obj), {});
 
 const propRegex = /([A-z-]+)[\t ]*((?:[\s]*\{[^}]*\})|(?:[\t ]*[^;\n]+))?/gm;
 
@@ -55,16 +165,14 @@ const cornerDirections = [
 	[['TopLeft'], ['TopRight'], ['BottomRight'], ['BottomLeft']]
 ];
 
-const processSides = (prefix, suffix='', dirs=directions) => (...args) => {
+const processSides = (prefix, suffix = '', dirs = directions) => (...args) => {
 	if (args.length <= 1) return { [prefix + suffix]: processValue(args[0]) };
 	return dirs[args.length - 1].reduce(
-		(obj, dir, i) => (
+		(obj, dir, i) =>
 			args[i]
-				? (dir.forEach(
-					d => (obj[prefix + d + suffix] = processValue(args[i]))
-				), obj)
-				: obj
-		), {}
+				? (dir.forEach(d => (obj[prefix + d + suffix] = processValue(args[i]))), obj)
+				: obj,
+		{}
 	);
 };
 
@@ -72,8 +180,17 @@ const helpers = {
 	...assignProps(processSides('margin'), 'margin', 'm'),
 	...assignProps(processSides('padding'), 'padding', 'p'),
 	...assignProps(processSides('border', 'Width'), 'borderWidth', 'border-width', 'bw'),
-	...assignProps(processSides('border', 'Radius', cornerDirections), 'borderRadius', 'border-radius', 'br'),
-	...assignProps((width, style, color) => rnss`bw ${width}; bs ${style}; border-color ${color}`, 'border', 'b')
+	...assignProps(
+		processSides('border', 'Radius', cornerDirections),
+		'borderRadius',
+		'border-radius',
+		'br'
+	),
+	...assignProps(
+		(width, style, color) => rnss`bw ${width}; bs ${style}; border-color ${color}`,
+		'border',
+		'b'
+	)
 };
 
 const createStyle = memo(str => {
@@ -85,7 +202,12 @@ const createStyle = memo(str => {
 
 		const helper = helpers[key];
 		if (helper) {
-			Object.assign(style, typeof helper === 'string' ? createStyle(helper) : helper(...(value || '').split(' ')));
+			Object.assign(
+				style,
+				typeof helper === 'string'
+					? createStyle(helper)
+					: helper(...(value || '').split(' '))
+			);
 			return;
 		}
 
@@ -100,8 +222,15 @@ const createStyle = memo(str => {
 
 export default function rnss(parts, ...args) {
 	if (typeof parts === 'string') return StyleSheet.create({ style: createStyle(parts) }).style;
-	else if (Array.isArray(parts)) return StyleSheet.create({ style: createStyle(zip(parts, args)) }).style;
-	else if (typeof parts === 'object') return StyleSheet.create(Object.entries(parts).reduce((obj, [key, value]) => (obj[key] = createStyle(value), obj), {}));
+	else if (Array.isArray(parts))
+		return StyleSheet.create({ style: createStyle(zip(parts, args)) }).style;
+	else if (typeof parts === 'object')
+		return StyleSheet.create(
+			Object.entries(parts).reduce(
+				(obj, [key, value]) => ((obj[key] = createStyle(value)), obj),
+				{}
+			)
+		);
 	return null;
 }
 rnss.helper = obj => Object.assign(helpers, obj);
